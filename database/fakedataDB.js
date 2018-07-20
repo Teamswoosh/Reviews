45a3051c44de585ca70c941f7c6678f2d4fb4b04
Monsoon <nepzsoon@gmail.com>
2018-07-20 12:32:06 -0700

    Testing ESlint

--- database/fakedataDB.js
+++ database/fakedataDB.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
 const db_connection = require('./db_connection.js')
 var faker = require('faker');
 
-const addData = () => {
+const addData = (review) => {
   var array = [review.user_name, review.date, review.review, review.rating, review.upvote, review.downvote]
-    db_connection.query('INSERT INTO user_reviews (name, review_date, review, stars, thumbs_up, thumbs_down) VALUES (?,?,?,?,?,?)', array, (err, data) => {
-      if (err) {
-        console.log('Error adding reviews to the database', err)
+  db_connection.query('INSERT INTO user_reviews (name, review_date, review, stars, thumbs_up, thumbs_down) VALUES (?,?,?,?,?,?)', array, (err, data) => {
+    if (err) {
+      console.log('Error adding reviews to the database', err)
       return
-      } 
-        console.log('Succesfully added to the database')
-      
-      })
-  }
+    }
+      console.log('Succesfully added to the database')  
+  })
+}
 
 const getData = (cb) => {
   db_connection.query('SELECT * FROM user_reviews', (err, data) => {
@@ -29,56 +27,49 @@ const getData = (cb) => {
 //Function generating fake reviews using faker.js 
 
 const generateFakeData = () => {
-=======
-var faker = require('faker');
-const db_connection = require('./db_connection.js')
 
-//Function generating fake reviews using faker.js 
+var arr = [];
+for (var i=0; i<10; i++) {
 
->>>>>>> 2cc8e25c95e5b596c49222e062172515438cdbdc
-for (var i=0; i<100; i++) {
+  var stars = faker.random.number({
+    'min': 1,
+    'max': 5
+  });
 
-var stars = faker.random.number({
-  'min': 1,
-  'max': 5
-});
+  var review = {
+    user_name: faker.name.findName(),
+    date: faker.date.month(),
+    review: faker.lorem.paragraph(),
+    rating: stars,
+    upvote: faker.random.number(),
+    downvote: faker.random.number()
+  };
 
-var review = {
-  user_name: faker.name.findName(),
-  date: faker.date.month(),
-  review: faker.lorem.paragraph(),
-  rating: stars,
-  upvote: faker.random.number(),
-  downvote: faker.random.number()
-};
-
-<<<<<<< HEAD
+  arr.push(review)
 }
+  CSV(arr)
 }
 
-//addData();
+function CSV(array) {
+  var keys = Object.keys(array[0]);
+  var result = keys.join("\t") + "\n";
+
+  array.forEach(function(obj){
+    keys.forEach(function(k, ix){
+      if (ix) result += "\t";
+        result += obj[k];
+      });
+    result += "\n";
+  });
+
+  console.log(result);
+}
+
+generateFakeData()
+
+//Dynamically adding each generated review into the mySql database
 
 module.exports = {
   addData,
   getData
 };
-=======
-
-//Dynamically adding each generated review into the mySql database
-const addData = () => {
-var array = [review.user_name, review.date, review.review, review.rating, review.upvote, review.downvote]
-  db_connection.query('INSERT INTO user_reviews (name, review_date, review, stars, thumbs_up, thumbs_down) VALUES (?,?,?,?,?,?)', array, (err, data) => {
-    if (err) {
-      console.log('Error adding reviews to the database', err)
-  	return
-    } else {
-  	console.log('Succesfully added to the database')
-    }
-  })
-}
-
-addData();
-
-};
-
->>>>>>> 2cc8e25c95e5b596c49222e062172515438cdbdc
